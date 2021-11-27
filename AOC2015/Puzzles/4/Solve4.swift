c25df905afb74f7f60626e9367cf6d5eec95f51b
Richard Pineo <Richard.E.Pineo@ge.com>
2021-11-27 12:07:42 -0800

    4b

--- AOC2015/Puzzles/4/Solve4.swift
+++ AOC2015/Puzzles/4/Solve4.swift
@@ -5,44 +5,49 @@ import SwiftUI
 
 class Solve4: PuzzleSolver {
 	func solveAExamples() -> Bool {
-		examplesA.allSatisfy { solveA(input: $0.input) == $0.answer }
+		examplesA.allSatisfy { solve(input: $0.input, pred: firstFiveBytesZero) == $0.answer }
 	}
 
 	func solveBExamples() -> Bool {
-		examplesB.allSatisfy { solveB(input: $0.input) == $0.answer }
+		examplesB.allSatisfy { solve(input: $0.input, pred: firstSixBytesZero) == $0.answer }
 	}
 	
 	static let input = "ckczppom"
 
 	var answerA = "117946"
-	var answerB = ""
+	var answerB = "3938038"
+
+	func firstFiveBytesZero(bytes: UnsafeRawBufferPointer) -> Bool {
+		bytes[0] == 0 && bytes[1] == 0 && bytes[2] < 16
+	}
+	func firstSixBytesZero(bytes: UnsafeRawBufferPointer) -> Bool {
+		bytes[0] == 0 && bytes[1] == 0 && bytes[2] == 0
+	}
 
 	func solveA() -> String {
-		solveA(input: Self.input).description
+		return solve(input: Self.input, pred: firstFiveBytesZero).description
 	}
 
 	func solveB() -> String {
-		""
+		return solve(input: Self.input, pred: firstSixBytesZero).description
 	}
+	
+	typealias Passes = (UnsafeRawBufferPointer) -> Bool
 
-	func solveA(input: String) -> Int {
+	func solve(input: String, pred: Passes) -> Int {
 		let max = 999999999
 		for value in 0...max {
 			let full = "\(input)\(value)".md5Data
 			
 			var found = false
 			full.withUnsafeBytes { bytes in
-				found = bytes[0] == 0 && bytes[1] == 0 && bytes[2] < 16
+				found = pred(bytes)
 			}
 			if found {
 				return value
 			}			 
 		}
 		return -666
-	}
-
-	func solveB(input: String) -> Int {
-		0
 	}
 
 	struct Example {
