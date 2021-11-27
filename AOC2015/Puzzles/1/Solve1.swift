af95ad5b2ecbd80eed6180745215e2169a942400
Richard Pineo <Richard.E.Pineo@ge.com>
2021-11-26 22:53:51 -0800

    1b

--- AOC2015/Puzzles/1/Solve1.swift
+++ AOC2015/Puzzles/1/Solve1.swift
@@ -4,32 +4,49 @@ import Foundation
 
 class Solve1: PuzzleSolver {
 	func solveAExamples() -> Bool {
-		examples.allSatisfy { solveA(input: $0.input) == $0.answer }
+		examplesA.allSatisfy { solveA(input: $0.input) == $0.answer }
 	}
 
 	func solveBExamples() -> Bool {
-		true
+		examplesB.allSatisfy { solveB(input: $0.input) == $0.answer }
 	}
 
 	var answerA = "232"
-	var answerB = ""
+	var answerB = "1783"
 
 	func solveA() -> String {
 		let file = FileHelper.load("Input1")
-		guard let line = file?[0] else {
-			return ""
-		}
-		return solveA(input: line).description
+		return solveA(input: file?[0] ?? "").description
 	}
 
 	func solveB() -> String {
-		""
+		let file = FileHelper.load("Input1")
+		return solveB(input: file?[0] ?? "").description
 	}
 
+	private let upDog: String.Element = "("
+	private let downDog: String.Element = ")"
+	
 	func solveA(input: String) -> Int {
-		let up = input.filter { $0 == "("}.count
-		let down = input.filter { $0 == ")"}.count
+		let up = input.filter { $0 == upDog}.count
+		let down = input.filter { $0 == downDog}.count
 		return up - down
+	}
+	
+	func solveB(input: String) -> Int {
+		var floor = 0
+		let pos = input.firstIndex {
+			if $0 == upDog {
+				floor += 1
+			} else if $0 == downDog {
+				floor -= 1
+			}
+			return floor < 0
+		}
+		guard let firstPos = pos else {
+			return 666
+		}
+		return input.distance(from: input.startIndex, to: firstPos) + 1
 	}
 
 	struct Example {
@@ -37,7 +54,7 @@ class Solve1: PuzzleSolver {
 		var answer: Int
 	}
 
-	let examples: [Example] = [
+	let examplesA: [Example] = [
 		.init(input: "(())", answer: 0),
 		.init(input: "()()", answer: 0),
 		.init(input: "(((", answer: 3),
@@ -48,4 +65,9 @@ class Solve1: PuzzleSolver {
 		.init(input: ")))", answer: -3),
 		.init(input: ")())())", answer: -3),
 	]
-}
+	
+
+	let examplesB: [Example] = [
+		.init(input: ")", answer: 1),
+		.init(input: "()())", answer: 5)
+	]}
