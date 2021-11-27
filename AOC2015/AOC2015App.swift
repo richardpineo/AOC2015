8632b63b6a3c1933fff709ef45bad0be0471513c
Richard Pineo <Richard.E.Pineo@ge.com>
2021-11-26 22:19:33 -0800

    Initial Commit

---
+++ AOC2015/AOC2015App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2015App.swift
+//  AOC2015
+//
+//  Created by Richard Pineo on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AOC2015App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AOC2015/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AOC2015
+//
+//  Created by Richard Pineo on 11/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
