685a1061aec9430fafab664d3477e6b542387987
Richard Pineo <Richard.E.Pineo@ge.com>
2021-11-28 15:49:56 -0800

    10b

--- AOC2015/Puzzles/10/Solve10.swift
+++ AOC2015/Puzzles/10/Solve10.swift
@@ -5,62 +5,55 @@ import SwiftUI
 
 class Solve10: PuzzleSolver {
 	func solveAExamples() -> Bool {
-		examplesA.allSatisfy { permute($0.input) == $0.output }
+		examplesA.allSatisfy { lookAndSay($0.input) == $0.output }
 	}
 
 	func solveBExamples() -> Bool {
 		true
-		// examplesB.allSatisfy { solve(input: $0.input, pred: firstSixBytesZero) == $0.answer }
 	}
 
 	static let input = "1321131112"
-	static let cycles = 40
 
 	var answerA = "492982"
 	var answerB = ""
 
 	func solveA() -> String {
-		solve(input: Self.input, cycles: Self.cycles).description
+		solve(input: Self.input, cycles: 40).description
 	}
 
 	func solveB() -> String {
-		""
-//		solve(input: Self.input, pred: firstSixBytesZero).description
-	}
-	
-	struct Entry {
-		var c: String.Element
-		var i: Int
-	}
-	
-	func permute(_ input: String) -> String {
-		var entries = Queue<Entry>()
-		var last = input.character(at: 0)
-		var count = 1
-		for i in 1..<input.count {
-			let c = input.character(at: i)
-			if c == last {
-				count += 1
-			} else {
-				entries.enqueue(.init(c: last, i: count))
-				last = c
-				count = 1
-			}
-		}
-		entries.enqueue(.init(c: last, i: count))
-		
-		return entries.array.map { "\($0.i)\($0.c)"}.joined(separator: "")
+		solve(input: Self.input, cycles: 50).description
 	}
 	
 	func solve(input: String, cycles: Int) -> Int {
 		var c = input
 		for i in 0..<cycles {
 			print("Round \(i): \(c.count)")
-			c = permute(c)
+			c = lookAndSay( c)
 		}
 		return c.count
 	}
-
+	
+	func lookAndSay(_ oldStr: String) -> String {
+		var new = Array<String>()
+		let old = Array(oldStr)
+		var repDigit = old[0]
+		var numRep = 1
+		for i in 1..<old.count {
+			if repDigit == old[i] {
+				numRep += 1
+			} else {
+				new.append(numRep.description)
+				new.append(repDigit.description)
+				repDigit = old[i]
+				numRep = 1
+			}
+		}
+		new.append(numRep.description)
+		new.append(repDigit.description)
+		return new.joined(separator: "")
+	}
+	
 	struct Example {
 		var input: String
 		var output: String
@@ -72,8 +65,5 @@ class Solve10: PuzzleSolver {
 		.init(input: "21", output: "1211"),
 		.init(input: "1211", output: "111221"),
 		.init(input: "111221", output: "312211"),
-	]
-
-	let examplesB: [Example] = [
 	]
 }
