3a82e910edcca1ef259d956b1e82c6bea2cb2a7c
Richard Pineo <Richard.E.Pineo@ge.com>
2021-11-30 19:52:30 -0800

    13b

--- AOC2015/Puzzles/13/Solve13.swift
+++ AOC2015/Puzzles/13/Solve13.swift
@@ -13,14 +13,34 @@ class Solve13: PuzzleSolver {
 	}
 
 	var answerA = "618"
-	var answerB = ""
+	var answerB = "601"
+	
+	var shouldTestB = false
 
 	func solveA() -> String {
 		solveA("Input13").description
 	}
 
 	func solveB() -> String {
-		""
+		guard let file = FileHelper.load("Input13") else {
+			return ""
+		}
+		var entries = file.filter { !$0.isEmpty }.map { parse($0) }
+		
+		var people = Set<String>()
+		entries.forEach { people.insert( $0.person ) }
+		
+		let me = "me"
+		people.insert(me)
+		people.forEach {
+			entries.append(.init(person: me, target: $0, happiness: 0))
+			entries.append(.init(person: $0, target: me, happiness: 0))
+		}
+
+		let options = Combinatorics.permutations(Array(people))
+
+		let happies = options.map { happiness($0, makeLookup(entries)) }
+		return happies.max()!.description
 	}
 	
 	struct Entry {
@@ -37,13 +57,20 @@ class Solve13: PuzzleSolver {
 		return .init(person: components[0], target: components[10].trimmingCharacters(in: ["."]), happiness: value * (isGain ? 1 : -1))
 	}
 	
-	func happiness(_ option: [String], _ entries: [Entry]) -> Int {
+	typealias Lookup = Dictionary<String, Int>
+	func makeLookup(_ entries: [Entry]) -> Lookup {
+		var lookup = Lookup()
+		entries.forEach { lookup[$0.person + $0.target] = $0.happiness }
+		return lookup
+	}
+	
+	func happiness(_ option: [String], _ lookup: Lookup) -> Int {
 		var score = 0
 		for i in 0 ..< option.count {
 			let person1 = option[i]
 			let person2 = option[i == option.count-1 ? 0 : i + 1]
-			score += entries.first( where: { $0.person == person1 && $0.target == person2 })!.happiness
-			score += entries.first( where: { $0.person == person2 && $0.target == person1 })!.happiness
+			score += lookup[person1 + person2]!
+			score += lookup[person2 + person1]!
 		}
 		return score
 	}
@@ -58,7 +85,8 @@ class Solve13: PuzzleSolver {
 		entries.forEach { people.insert( $0.person ) }
 
 		let options = Combinatorics.permutations(Array(people))
-		let happies = options.map { happiness($0, entries) }
+		
+		let happies = options.map { happiness($0, makeLookup(entries)) }
 		return happies.max()!
 	}
 }
