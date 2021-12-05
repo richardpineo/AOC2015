91cf4709396bf31a4e2e531605d5098776918945
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-05 13:36:18 -0800

    15 brute force

--- AOC2015/Puzzles/15/Solve15.swift
+++ AOC2015/Puzzles/15/Solve15.swift
@@ -5,7 +5,7 @@ import SwiftUI
 
 class Solve15: PuzzleSolver {
 	func solveAExamples() -> Bool {
-		solveA("Example15") == 62_842_880
+		solveAExamples() == 62_842_880
 	}
 
 	func solveBExamples() -> Bool {
@@ -14,9 +14,68 @@ class Solve15: PuzzleSolver {
 
 	var answerA = "4"
 	var answerB = ""
+	
+	func solveAExamples() -> Int {
+		
+//	Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+//	Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+
+		
+		let butterscotch = Entry(ingredient: "Butterscotch", capacity: -1, durability: -2, flavor: 6, texture: 3, calories: 8)
+		let cinnamon = Entry(ingredient: "Cinnamon", capacity: 2, durability: 3, flavor: -2, texture: -1, calories: 3)
+
+		var maxVal = 0
+		for bu in 1...99 {
+//		let bu = 44
+			let ci = 100 - bu
+			if ci > 0 {
+				let cap = bu * butterscotch.capacity + ci * cinnamon.capacity
+				let dur = bu * butterscotch.durability + ci * cinnamon.durability
+				let fla = bu * butterscotch.flavor + ci * cinnamon.flavor
+				let tex = bu * butterscotch.texture + ci * cinnamon.texture
+				var val = cap * dur * fla * tex
+				if cap < 0 || dur < 0 || fla < 0 || tex < 0 {
+					val = 0
+				}
+				maxVal = max(maxVal, val)
+			}
+		}
+		return maxVal
+	}
 
 	func solveA() -> String {
-		solveA("Input15").description
+	// Sugar: capacity 3, durability 0, flavor 0, texture -3, calories 2
+	// Sprinkles: capacity -3, durability 3, flavor 0, texture 0, calories 9
+	// Candy: capacity -1, durability 0, flavor 4, texture 0, calories 1
+	// Chocolate: capacity 0, durability 0, flavor -2, texture 2, calories 8
+
+		let sugar = Entry(ingredient: "Sugar", capacity: 3, durability: 0, flavor: 0, texture: -3, calories: 2)
+		let sprinkles = Entry(ingredient: "Sprinkles", capacity: -3, durability: 3, flavor: 0, texture: 0, calories: 9)
+		let candy = Entry(ingredient: "Candy", capacity: -1, durability: 0, flavor: 4, texture: 0, calories: 1)
+		let chocolate = Entry(ingredient: "Chocolate", capacity: 0, durability: 0, flavor: -2, texture: 2, calories: 8)
+		
+		var maxVal = 0
+		for su in 1...99 {
+			for sp in 1...99 {
+				for ca in 1...99 {
+					let ch = 100 - ca - sp - su
+					if ch > 0 {
+						let cap = su * sugar.capacity + sp * sprinkles.capacity + ca * candy.capacity + ch * chocolate.capacity
+						let dur = su * sugar.durability + sp * sprinkles.durability + ca * candy.durability + ch * chocolate.durability
+						let fla = su * sugar.flavor + sp * sprinkles.flavor + ca * candy.flavor + ch * chocolate.flavor
+						let tex = su * sugar.texture + sp * sprinkles.texture + ca * candy.texture + ch * chocolate.texture
+						
+						var val = cap * dur * fla * tex
+						if cap < 0 || dur < 0 || fla < 0 || tex < 0 {
+							val = 0
+						}
+
+						maxVal = max(maxVal, val)
+					}
+				}
+			}
+		}
+		return maxVal.description
 	}
 
 	func solveB() -> String {
