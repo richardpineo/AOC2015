59a11b8357e941f18c895f0c77ca08d2e88ae4a4
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-05 22:57:46 -0800

    hacky

--- AOC2015/Puzzles/19/Solve19.swift
+++ AOC2015/Puzzles/19/Solve19.swift
@@ -5,23 +5,22 @@ import SwiftUI
 
 class Solve19: PuzzleSolver {
 	func solveAExamples() -> Bool {
-		solve(file: "Example19-1") == 4 && solve(file: "Example19-2") == 7
+		solveA(file: "Example19-1") == 4 && solveA(file: "Example19-2") == 7
 	}
 
 	func solveBExamples() -> Bool {
-		true
+		solveB(file: "Example19-3") == 3 && solveB(file: "Example19-4") == 6
 	}
 
 	var answerA = "576"
 	var answerB = ""
 
 	func solveA() -> String {
-		solve(file: "Input19").description
+		solveA(file: "Input19").description
 	}
 
 	func solveB() -> String {
-		""
-//		solve(file: "Input18", dim: 100, rounds: 100, mutate: Solve18.cornersOn).description
+		solveB(file: "Input19").description
 	}
 	
 	struct Rule {
@@ -36,7 +35,7 @@ class Solve19: PuzzleSolver {
 		return molecules
 	}
 	
-	func solve(file: String) -> Int {
+	func readFile(_ file: String) -> ([Rule], String) {
 		let lines = FileHelper.load(file)!
 		
 		var rules: [Rule] = []
@@ -49,13 +48,42 @@ class Solve19: PuzzleSolver {
 		}
 		
 		let start = lines[index + 1]
-		
+		return (rules, start)
+	}
+	
+	func solveA(file: String) -> Int {
+		let input = readFile(file)
 		var mutations = Set<String>()
-		rules.forEach {
-			let generated = generateMolecules(start: start, rule: $0)
-			generated.forEach { mutations.insert($0) }
+		input.0.forEach {
+			mutations.formUnion(  generateMolecules(start: input.1, rule: $0))
+		}
+		return mutations.count
+	}
+	
+	func solveB(file: String) -> Int {
+		let (rules, key) = readFile(file)
+		var mutations = Set<String>()
+		mutations.insert("e")
+		
+		var round = 0
+		while !mutations.contains(key) {
+			var newMutations = Set<String>()
+			rules.forEach { rule in
+				mutations.forEach { mutation in
+					let generated = generateMolecules(start: mutation, rule: rule)
+					if !generated.isEmpty {
+						let filtered = generated.filter {
+							!mutations.contains($0) && $0.count <= key.count
+						}
+						newMutations.formUnion(filtered)
+					}
+				}
+			}
+			mutations.formUnion(newMutations)
+			round += 1
+			print("After \(round) rounds there are \(mutations.count)")
 		}
 		
-		return mutations.count
+		return round
 	}
 }
